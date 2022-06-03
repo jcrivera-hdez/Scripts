22e8467bfcd10cc77ad01c9b3449f897ed87fd92
jcrivera <jcrivera@kth.se>
2022-06-03 13:27:48 +0200

    Update jpa-gain-plot.py
    Updated functions, and replace last figure code by a more optimized one

--- data-analysis/JPA/jpa-gain-plot.py
+++ data-analysis/JPA/jpa-gain-plot.py
@@ -7,17 +7,19 @@ import matplotlib.pyplot as plt
 def dB( data ):
     return 20 * np.log10( np.abs(data**2) )
 
-def plot_traces( trace, freq_arr ):
-    ax.plot( freq_arr/1e9, dB(trace) )
+
+def plot_traces( trace, freq_arr, ax ):
+    ax.plot( freq_arr/1e9, dB(trace), label=f'{pump_pwr_arr[pump_idx+1]:.3f} fsu' )
+    ax.set_title( r'DC bias ' + f'= {bias_arr[bias_ind]:.3f} V' )
     ax.set_xlabel( 'frequency [GHz]' )
     ax.set_ylabel( 'gain [dB]' )
 
-def plot_gain( gain, freq_arr, bias_arr ):
-    cutoff = 0.1  # %
+
+def plot_gain( gain, freq_arr, bias_arr, fig, ax ):
+    cutoff = 0.1
     zmin = np.percentile( gain, cutoff )
     zmax = np.percentile( gain, 100. - cutoff )
     
-    fig, ax = plt.subplots( 1 )
     a = ax.pcolormesh(bias_arr,
                       freq_arr/1e9, 
                       gain.T, 
@@ -28,6 +30,7 @@ def plot_gain( gain, freq_arr, bias_arr ):
     fig.colorbar( a, label=r'gain [dB]' )
     ax.set_xlabel( 'DC bias [V]' )
     ax.set_ylabel( 'frequency [GHz]' )
+
 
 def plot_gainsweep( gain_arr, freq_arr, bias_arr ):
     cutoff = 0.1  # %
@@ -82,31 +85,22 @@ bias_ind = 19
 
 # Amplitude normalised
 gain_ref = dB(usb_arr[0])
-gain_arr = dB(usb_arr[1:])  - dB(usb_arr[0])
+gain_arr = dB(usb_arr[1:])  - gain_ref
 
 
-# PLOTTING
-
-# Background (no-pump) response
-fig, ax = plt.subplots(1)
-plot_traces( usb_arr[0][bias_ind], freq_arr )
+# Plot background (no-pump) response
+pump_idx = 0
+fig0, ax0 = plt.subplots(1)
+plot_traces( usb_arr[pump_idx][bias_ind], freq_arr, ax0 )
 
 # Plot gain at a given pump power
-plot_gain( gain_arr[-1], freq_arr, bias_arr )
+fig1, ax1 = plt.subplots(1)
+plot_gain( gain_arr[-1], freq_arr, bias_arr, fig1, ax1 )
 
 # Plot gain sweep for all pump powers
 plot_gainsweep( gain_arr, freq_arr, bias_arr )
 
-fig, ax = plt.subplots(1)
+fig2, ax2 = plt.subplots(1)
 for pump_idx in range(nr_pump_pwr-1):
-    plot_traces( usb_arr[pump_idx, bias_ind], freq_arr )
-
-# JPA linewidth for a given dc bias and JPA pump power.
-
-fig, ax = plt.subplots( 1 )
-for i in range(nr_pump_pwr-1):
-    ax.plot( freq_arr/1e9, gain_arr[i,bias_ind], label=f'{pump_pwr_arr[i+1]:.3f} fsu' )
-ax.set_title( r'DC bias ' + f'= {bias_arr[bias_ind]:.3f} V' )
-ax.set_xlabel( 'frequency [GHz]' )
-ax.set_ylabel( 'gain [dB]' )
-ax.legend( title=r'$A_p$' )
+    plot_traces( usb_arr[pump_idx, bias_ind], freq_arr, ax2 )
+ax2.legend( title=r'$A_p$' )
