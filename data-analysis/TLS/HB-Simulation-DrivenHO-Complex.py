387e15da7b64188da2baaf8f50ea7f6d3f8771b9
jcrivera <jcrivera@kth.se>
2022-06-15 10:20:02 +0200

    Update HB-Simulation-DrivenHO-Complex.py
    Function for reconstruction algorithm

--- data-analysis/TLS/HB-Simulation-DrivenHO-Complex.py
+++ data-analysis/TLS/HB-Simulation-DrivenHO-Complex.py
@@ -10,15 +10,17 @@ import scipy.linalg
 
 π = np.pi
 
-
+# Functions
 def eom( t, state, fr, κ0, drive ):
     ωr = 2*π * fr
     return -1.0j*ωr*state - κ0*state + drive(t)
+
 
 def imp_drive( t, f1, f2, F0, m ):
     ω1 = 2*π * f1
     ω2 = 2*π * f2
     return F0 * ( np.cos(ω1*t) + np.cos(ω2*t) )
+
 
 def eom_real( t, state_real, fr, κ0, drive ):
     state_complex = state_real[::2] + 1j*state_real[1::2]
@@ -29,7 +31,59 @@ def eom_real( t, state_real, fr, κ0, drive ):
     return tmp
 
 
-# Drive signal
+def reconstruction( A, f ):
+    # Angular frequency
+    ω = 2*π * f
+    
+    # Create H-matrix
+    col1 = (1.0j * ω * A)[max_ind[0]]
+    col2 = (1.0j * A)[max_ind[0]]
+    col3 = A[max_ind[0]]
+    
+    # Merge all columns
+    H = np.vstack(( col1, col2, col3 ))
+    
+    # Making the matrix real instead of complex
+    Hcos = np.real( H )
+    Hsin = np.imag( H )
+    H = np.hstack(( Hcos, Hsin ))
+    
+    # Normalize H for a more stable inversion
+    Nm = np.diag( 1. / np.max(np.abs(H), axis=1) )
+    H_norm = np.dot( Nm, H )  # normalized H-matrix
+    
+    # The drive vector, Q (from the Yasuda paper)
+    Qcos = np.real( signal_amp_array_imp )
+    Qsin = np.imag( signal_amp_array_imp ) 
+    Q = np.hstack(( Qcos, Qsin ))
+    
+    # Solve system Q = H*p
+    H_norm_inv = scipy.linalg.pinv( H_norm )
+    p_norm = np.dot( Q, H_norm_inv )
+    
+    # Re-normalize p-values
+    # Note: we have actually solved Q = H * Nm * Ninv * p
+    # Thus we obtained Ninv*p and multiply by Nm to obtain p
+    p = np.dot( Nm, p_norm )  # re-normalize parameter values
+    
+    # Forward calculation to check result, should be almost everything zero vector
+    Q_fit = np.dot( p, H )
+    
+    # Scale parameters by drive force assuming known mass
+    AA, B, C = p
+    
+    # Assuming a known drive
+    m_recon = 1.
+    
+    # Parameters reconstructed
+    F0_recon = 1/AA
+    f0_recon = F0_recon * B / (2*π)
+    κ0_recon = F0_recon * C
+    
+    return F0_recon, f0_recon, κ0_recon
+
+
+# Drive parameters
 F0 = 1
 f1 = 0.954
 f2 = 0.956
@@ -37,6 +91,7 @@ df = 0.001
 fs = 20
 N = int(round(fs/df))
 
+# Integration parameters
 T = 1. / df
 T_relax = 10 * T
 dt = 1. / fs
@@ -96,7 +151,7 @@ ax[2].set_ylabel( '$|\mathcal{F}(a)|$' )
 ax[2].set_ylim( 1e-8, 1e2 )
 
 
-# Indices of the amplitude minima
+# Indices of the amplitude maxima
 max_ind = find_peaks( x = np.abs(A),
                       height = 2e-2,
                       )
@@ -125,56 +180,9 @@ for i, index in enumerate( max_ind[0] ):
 # ax2.set_ylabel( '$\mathcal{F}(x)$' )
 
 
-# Harmonic Balance section
-
-# Angular frequency
-ω = 2*π * f
-
-# Create H-matrix
-col1 = (1.0j * ω * A)[max_ind[0]]
-col2 = (1.0j * A)[max_ind[0]]
-col3 = A[max_ind[0]]
-
-# Merge all columns
-H = np.vstack(( col1, col2, col3 ))
-
-# Making the matrix real instead of complex
-Hcos = np.real( H )
-Hsin = np.imag( H )
-H = np.hstack(( Hcos, Hsin ))
-
-# Normalize H for a more stable inversion
-Nm = np.diag( 1. / np.max(np.abs(H), axis=1) )
-H_norm = np.dot( Nm, H )  # normalized H-matrix
-
-# The drive vector, Q (from the Yasuda paper)
-Qcos = np.real( signal_amp_array_imp )
-Qsin = np.imag( signal_amp_array_imp ) 
-Q = np.hstack(( Qcos, Qsin ))
-
-# Solve system Q = H*p
-H_norm_inv = scipy.linalg.pinv( H_norm )
-p_norm = np.dot( Q, H_norm_inv )
-
-# Re-normalize p-values
-# Note: we have actually solved Q = H * Nm * Ninv * p
-# Thus we obtained Ninv*p and multiply by Nm to obtain p
-p = np.dot( Nm, p_norm )  # re-normalize parameter values
-
-# Forward calculation to check result, should be almost everything zero vector
-Q_fit = np.dot( p, H )
-
-# Scale parameters by drive force assuming known mass
-AA, B, C = p
-
-# Assuming a known drive
-m_recon = 1.
-
-# Parameters reconstructed
-F0_recon = 1/AA
-f0_recon = F0_recon * B / (2*π)
-κ0_recon = F0_recon * C
-
+# Reconstruction section
+F0_recon, f0_recon, κ0_recon = reconstruction( A, f )
+    
 print( "f0_recon = {} Hz".format(f0_recon) )
 print( "κ0_recon = {}".format(κ0_recon) )
 print( "F0 = {}".format(F0_recon) )
